d2607a982a1b23a8c92f9464c29bff9a142d648a
Devraaj Singh Gill <117158829+DevraajSinghGill@users.noreply.github.com>
2024-07-06 12:47:54 +0800

    Revert "Revert "Revert "Commit"""
    This reverts commit 565ac164e7d9f8b7388830eb9fec4a16838363bf.

--- lib/screens/custom_navbar/chat/group/group_chat_screen.dart
+++ lib/screens/custom_navbar/chat/group/group_chat_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:terra_zero_waste_app/constants/app_text_styles.dart';
-import 'package:terra_zero_waste_app/gamification/group_task_gamification/group_task_list.dart';
-import 'package:terra_zero_waste_app/gamification/group_task_gamification/group_task_screen.dart';
 import 'package:terra_zero_waste_app/screens/custom_navbar/chat/group/widgets/group_chat_portion.dart';
 import 'package:terra_zero_waste_app/screens/custom_navbar/chat/group/widgets/image_sending_portion_in_group.dart';
 import 'package:terra_zero_waste_app/services/group_chat_services.dart';
@@ -10,8 +8,6 @@ import 'package:terra_zero_waste_app/widgets/text_inputs.dart';
 import 'package:terra_zero_waste_app/controllers/image_controller.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:terra_zero_waste_app/gamification/group_task_gamification/group_task_creation.dart';
- // Ensure this import points to the correct file
 
 class GroupChatScreen extends StatefulWidget {
   final String groupId;
@@ -25,7 +21,6 @@ class _GroupChatScreenState extends State<GroupChatScreen> {
   final TextEditingController msgController = TextEditingController();
 
   bool _isLoading = false;
-
   @override
   Widget build(BuildContext context) {
     final imageController = Provider.of<ImageController>(context);
@@ -36,26 +31,6 @@ class _GroupChatScreenState extends State<GroupChatScreen> {
           "Group Chat",
           style: AppTextStyles.nunitoBold.copyWith(color: Colors.white),
         ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => GroupCreateTaskScreen(groupId: widget.groupId)),
-              );
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.list),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => GroupTasksScreen(groupId: widget.groupId)),
-              );
-            },
-          ),
-        ],
       ),
       bottomNavigationBar: imageController.selectedImage != null
           ? SizedBox()
@@ -65,12 +40,14 @@ class _GroupChatScreenState extends State<GroupChatScreen> {
                 _sendChat(context, imageController, msgController);
               },
             ),
-      body: Column(
-        children: [
-          Expanded(child: GroupChatPortion(groupId: widget.groupId)),
-          GroupTaskList(groupId: widget.groupId),
-        ],
-      ),
+      body: imageController.selectedImage != null
+          ? ImageSendingPortionInGroup(
+              imageController: imageController,
+              onSendBtnClick: () {
+                _sendChat(context, imageController, msgController);
+              },
+              isLoading: _isLoading)
+          : GroupChatPortion(groupId: widget.groupId),
     );
   }
 

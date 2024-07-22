7509a20d34bfcb857ba33d1d294272b39d8ccf5f
Devraaj Singh Gill <117158829+DevraajSinghGill@users.noreply.github.com>
2024-07-23 01:05:38 +0800

    Commit

--- lib/screens/auth/forgot_password_screen.dart
+++ lib/screens/auth/forgot_password_screen.dart
@@ -27,54 +27,57 @@ class ForgotPasswordScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 1,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 20.sp),
-              const Center(
-                child: SafeArea(
-                  child: LogoWidget(),
+      body: Container(
+        color: Colors.white,  // Set the background color to white
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 20.sp),
+                const Center(
+                  child: SafeArea(
+                    child: LogoWidget(),
+                  ),
                 ),
-              ),
-              SizedBox(height: 30.h),
-              Text("Reset your password",
-                  style: AppTextStyles.nunitoSemiBod.copyWith(fontWeight: FontWeight.w900)),
-              const SizedBox(height: 5),
-              Text(
-                "Please Enter your email to get link for password reset",
-                style: AppTextStyles.nunitoSemiBod.copyWith(fontSize: 14),
-              ),
-              SizedBox(height: 40.h),
-              CustomTextInput(
-                controller: AppTextController.emailController,
-                hintText: "E-mail",
-              ),
-              SizedBox(height: 20.h),  // Reduced space between email input and reset button
-              Consumer<LoadingController>(builder: (context, loadingController, child) {
-                return loadingController.isLoading
-                    ? Center(
-                        child: CircularProgressIndicator(color: AppColors.primaryColor),
-                      )
-                    : Column(
-                        children: [
-                          PrimaryButton(
-                            onPressed: () {
-                              AuthServices()
-                                  .resetPassword(context, AppTextController.emailController.text)
-                                  .whenComplete(() {
-                                AppTextController().clearTextInput();
-                              });
-                            },
-                            title: "Reset",
-                          ),
-                          SizedBox(height: 20.h),  // Added space between reset button and bottom of the screen
-                        ],
-                      );
-              }),
-            ],
+                SizedBox(height: 30.h),
+                Text("Reset your password",
+                    style: AppTextStyles.nunitoSemiBod.copyWith(fontWeight: FontWeight.w900)),
+                const SizedBox(height: 5),
+                Text(
+                  "Please Enter your email to get link for password reset",
+                  style: AppTextStyles.nunitoSemiBod.copyWith(fontSize: 14),
+                ),
+                SizedBox(height: 40.h),
+                CustomTextInput(
+                  controller: AppTextController.emailController,
+                  hintText: "E-mail",
+                ),
+                SizedBox(height: 20.h),  // Reduced space between email input and reset button
+                Consumer<LoadingController>(builder: (context, loadingController, child) {
+                  return loadingController.isLoading
+                      ? Center(
+                          child: CircularProgressIndicator(color: AppColors.primaryColor),
+                        )
+                      : Column(
+                          children: [
+                            PrimaryButton(
+                              onPressed: () {
+                                AuthServices()
+                                    .resetPassword(context, AppTextController.emailController.text)
+                                    .whenComplete(() {
+                                  AppTextController().clearTextInput();
+                                });
+                              },
+                              title: "Reset",
+                            ),
+                            SizedBox(height: 20.h),  // Added space between reset button and bottom of the screen
+                          ],
+                        );
+                }),
+              ],
+            ),
           ),
         ),
       ),
